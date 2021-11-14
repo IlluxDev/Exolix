ecf46ebf52216af06e2bf7e070977939d1f56486
XfaonAE <xfaonae@gmail.com>
2021-11-14 00:29:08 -0500

    u

--- test.ts
+++ test.ts
@@ -10,12 +10,20 @@ const rest = new Rest({
 	port: 8080,
 });
 
+let list = ["Walk the dog"]
+
+function buildList(): string {
+	let opt = "";
+	list.forEach
+}
+
 rest.onRequest("get", "/", (conn) => {
 	console.log("New request");
 	conn.write(
 		"<h1>Welcome to this website</h1>" +
 			'<form method="POST" action="/">' +
 			'<input name="name" placeholder="Name" />' +
+			buildList() +
 			"<button>Submit</button>" +
 			"</form>" +
 			"<p>Footer | Powered by Exolix REST Api</p>"
