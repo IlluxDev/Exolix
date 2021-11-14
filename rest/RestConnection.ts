492825ec45c9a751b9debdf4636c5e086b519fcc
XFaon <85759101+XFaonAE@users.noreply.github.com>
2021-11-13 21:25:09 -0500

    Added get express data

--- rest/RestConnection.ts
+++ rest/RestConnection.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
 
 export class RestConnection {
-	public constructor(request: Request, response: Response) {
+	private expressData: { request: Request, response: Response };
 
+	public constructor(request: Request, response: Response) {
+		this.expressData = {request, response};
+	}
+
+	public getExpressResponse(): Response {
+		return this.expressData.response;
+	}
+
+	public getExpressRequest(): Request {
+		return this.expressData.request;
 	}
 }
